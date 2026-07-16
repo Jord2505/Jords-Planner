cb805022f3853b8d87fb7f5163f12c54ce5a937d
Jord2505 <jordancallaghan53@gmail.com>
2026-07-16 10:36:26 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
+const cacheName = "jords-planner-v1";
 
+const files = [
+"index.html",
+"style.css",
+"app.js",
+"manifest.json"
+];
+
+
+self.addEventListener(
+"install",
+event => {
+
+event.waitUntil(
+
+caches.open(cacheName)
+.then(cache =>
+cache.addAll(files)
+)
+
+);
+
+});
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response =>
+response || fetch(event.request)
+)
+
+);
+
+});
